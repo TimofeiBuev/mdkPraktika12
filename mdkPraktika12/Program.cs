00862ddba8e63cd84292fb75accf683f32ddb33d
Timofei <timofeibuev234@gmail.com>
2025-05-26 12:00:09 +0500

    Добавьте файлы проекта.

---
+++ mdkPraktika12/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace mdkPraktika12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Выбирете функцию");
+                Console.WriteLine("- ввести А \n- ввести В \n- выполнить операцию “+” \n- выполнить операцию “-” \n- выполнить операцию “*” \n- выполнить операцию “/” ");
+
+                int chose = Convert.ToInt32(Console.ReadLine());
+
+                double a = 0;
+                double b = 0;
+
+                switch (chose)
+                {
+                    case 1:
+                        a = Convert.ToDouble(Console.ReadLine());
+                        break;
+                    case 2:
+                        b = Convert.ToDouble(Console.ReadLine());
+                        break;
+                    case 3:
+                        Console.WriteLine(a + b);
+                        break;
+                    case 4:
+                        Console.WriteLine(a - b);
+                        break;
+                    case 5:
+                        Console.WriteLine(a * b);
+                        break;
+                    case 6:
+                        Console.WriteLine(a / b);
+                        break;
+                    default:
+                        Console.WriteLine("Введены неверные данные");
+                        break;
+                }
+            }
+        }
+    }
+}
