fb443770aae5786c42671e97074b546e58a62450
Timofei <timofeibuev234@gmail.com>
2025-05-26 12:17:33 +0500

    +

--- mdkPraktika12/Program.cs
+++ mdkPraktika12/Program.cs
@@ -16,23 +16,8 @@
 
                 switch (chose)
                 {
-                    case 1:
-                        a = Convert.ToDouble(Console.ReadLine());
-                        break;
-                    case 2:
-                        b = Convert.ToDouble(Console.ReadLine());
-                        break;
                     case 3:
                         Console.WriteLine(a + b);
-                        break;
-                    case 4:
-                        Console.WriteLine(a - b);
-                        break;
-                    case 5:
-                        Console.WriteLine(a * b);
-                        break;
-                    case 6:
-                        Console.WriteLine(a / b);
                         break;
                     default:
                         Console.WriteLine("Введены неверные данные");
