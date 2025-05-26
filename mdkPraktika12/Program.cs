f91e967a16c3c3059152d3de464b90e3cc5f9b02
Timofei <timofeibuev234@gmail.com>
2025-05-26 12:18:52 +0500

    разделить

--- mdkPraktika12/Program.cs
+++ mdkPraktika12/Program.cs
@@ -16,21 +16,6 @@
 
                 switch (chose)
                 {
-                    case 1:
-                        a = Convert.ToDouble(Console.ReadLine());
-                        break;
-                    case 2:
-                        b = Convert.ToDouble(Console.ReadLine());
-                        break;
-                    case 3:
-                        Console.WriteLine(a + b);
-                        break;
-                    case 4:
-                        Console.WriteLine(a - b);
-                        break;
-                    case 5:
-                        Console.WriteLine(a * b);
-                        break;
                     case 6:
                         Console.WriteLine(a / b);
                         break;
